baea0ff61475b6b64434f1a420182799ed1c04fb
Jan Wyszynski <jan.wyszynski@joyent.com>
2018-07-21 01:38:53 +0000

    joyent/manta-garbage-collector#27 improve reader and cleaner test coverage

--- test/moray_delete_record_reader.test.js
+++ test/moray_delete_record_reader.test.js
@@ -310,6 +310,108 @@ do_filter_test(test_done)
 }
 
 
+function
+do_pause_resume_test(test_done)
+{
+	mod_vasync.waterfall([
+		function setup_context(next) {
+			lib_testcommon.create_mock_context(function (err, ctx) {
+				if (err) {
+					console.log('error creating context');
+					next(err);
+					return;
+				}
+
+				ctx.ctx_cfg.creators = [
+					{
+						uuid: TEST_OWNER
+					}
+				];
+
+				var shard = Object.keys(ctx.ctx_moray_clients)[0];
+
+				next(null, ctx, shard);
+			});
+		},
+		function create_and_pause_reader(ctx, shard, next) {
+			var listener = new mod_events.EventEmitter();
+			var reader = lib_testcommon.create_moray_delete_record_reader(
+				ctx, shard, listener);
+			var timer = setTimeout(function () {
+				mod_assertplus.ok(false, 'did not receive ' +
+					'paused event');
+			}, DELAY);
+			reader.once('paused', function () {
+				clearTimeout(timer);
+				next(null, ctx, shard, reader, listener);
+			});
+			reader.emit('assertPause');
+		},
+		function create_fake_record(ctx, shard, reader, listener, next) {
+			var client = ctx.ctx_moray_clients[shard];
+			lib_testcommon.create_fake_delete_record(
+				ctx,
+				client,
+				BUCKET,
+				TEST_OWNER,
+				OBJECTS[TEST_OWNER],
+				[SHARK],
+				function (err) {
+				if (err) {
+					ctx.ctx_log.error(err, 'unabled to create delete record');
+					next(err);
+					return;
+				}
+				next(null, ctx, shard, reader, listener);
+			});
+		},
+		function listen_for_record_while_paused(ctx, shard, reader, listener, next) {
+			listener.on('record', function () {
+				mod_assertplus.ok(false, 'unexpected record event while ' +
+					'reader is paused');
+			});
+			setTimeout(function () {
+				listener.removeAllListeners('record');
+				next(null, ctx, shard, reader, listener);
+			}, DELAY);
+		},
+		function resume_reader(ctx, shard, reader, listener, next) {
+			setTimeout(function () {
+				mod_assertplus.ok(false, 'did not receive running ' +
+					'event after resuming reader');
+			}, DELAY);
+			reader.once('running', function () {
+				next(null, ctx, shard, reader, listener);
+			});
+			reader.emit('assertResume');
+		},
+		function listen_for_records(ctx, shard, reader, listener, next) {
+			var timer = setTimeout(function () {
+				mod_assertplus.ok(false, 'did not receive record ' +
+					'after resuming reader');
+			}, DELAY);
+			listener.once('record', function (record) {
+				clearTimeout(timer);
+				next(null, ctx, shard);
+			});
+		},
+		function remove_record(ctx, shard, next) {
+			var client = ctx.ctx_moray_clients[shard];
+			lib_testcommon.remove_fake_delete_record(
+				ctx,
+				client,
+				mod_path.join(TEST_OBJECTID, SHARK),
+				next);
+		}
+	], function (err) {
+		if (err) {
+			process.exit(1);
+		}
+		test_done();
+	});
+}
+
+
 mod_vasync.pipeline({ funcs: [
 	function (_, next) {
 		do_basic_test(next);
@@ -319,6 +421,9 @@ mod_vasync.pipeline({ funcs: [
 	},
 	function (_, next) {
 		do_filter_test(next);
+	},
+	function (_, next) {
+		do_pause_resume_test(next);
 	}
 ]}, function (err) {
 	if (err) {
